cb5ea11fe63ccff75ab7aa2788085eaa6f1d6bb4
Rodrigo Araos <rodrigoaraosq2011@gmail.com>
2022-08-24 23:38:05 -0400

    Setup inicial del proyecto

---
+++ src/main/java/com/mycompany/proyectosia/ici2241/JavaMain.java
@@ -0,0 +1,13 @@
+package com.mycompany.proyectosia.ici2241;
+
+/**
+ *
+ * @author rodri
+ */
+public class JavaMain 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+    }
+}
